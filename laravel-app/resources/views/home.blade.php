513ff0c3cd5dbcae624a01b3973d6c92edab764b
AyushiAnand70047 <ayushianand088@gmail.com>
2024-09-09 15:14:37 +0530

    Routing practice

---
+++ laravel-app/resources/views/home.blade.php
@@ -0,0 +1,5 @@
+<h1>This is home page</h1>
+
+<a href="/">Welcome Page</a>
+<a href="/about">About Page</a>
+<a href="/contact/Ayushi">Contact Page</a>
--- laravel-app/routes/web.php
+++ laravel-app/routes/web.php
@@ -5,3 +5,20 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/home',function(){
+    return view('home');
+});
+
+Route::view('/about','about');
+
+Route::get('/contact/{name}',function($name){
+    // echo $name;
+    return view('contact',['name'=>$name]);
+});
+
+Route::get('/signup',function(){
+    return view('signup');
+});
+
+Route::redirect('/login','/signup');
