bdb5c5910919eed4af466fbfcbbd5d68b5b5a298
AyushiAnand70047 <ayushianand088@gmail.com>
2024-09-14 11:22:41 +0530

    form handling with checkbox

--- laravel-app/app/Http/Controllers/UserformController.php
+++ laravel-app/app/Http/Controllers/UserformController.php
@@ -12,6 +12,12 @@ class UserformController extends Controller
         echo $request->username . "<br/>";
         echo $request->useremail . "<br/>";
         echo $request->usercity . "<br/>";
+        // echo $request->skill . "<br/>";
+        print_r($request->skill);
+        echo "<br/>";
+        echo $request->gender . "<br/>";
+        echo $request->city . "<br/>";
+        echo $request->age . "<br/>";
         // return $request;
     }
 }
--- laravel-app/resources/views/user-form.blade.php
+++ laravel-app/resources/views/user-form.blade.php
@@ -12,6 +12,34 @@
             <input type="text" placeholder="Enter User City: " name="usercity"/>
         </div>
         <div class="input-wrapper">
+            <h4>User Skill</h4>
+            <input type="checkbox" name="skill[]" id="php" value="PHP"/>
+            <label for="php">PHP</label>
+            <input type="checkbox" name="skill[]" id="Laravel" value="Laravel"/>
+            <label for="Laravel">Laravel</label>
+            <input type="checkbox" name="skill[]" id="sql" value="SQL"/>
+            <label for="sql">SQL</label>
+        </div>
+        <div class="input-wrapper">
+            <h4>User Gender</h4>
+            <input type="radio" name="gender" id="male" value="Male"/>
+            <label for="male">Male</label>
+            <input type="radio" name="gender" id="female" value="Female"/>
+            <label for="female">Female</label>
+        </div>
+        <div class="input-wrapper">
+            <h4>User City</h4>
+            <select name="city">
+                <option value="Delhi">Delhi</option>
+                <option value="Noida">Noida</option>
+                <option value="Gurugram">Gurugram</option>
+            </select>
+        </div>
+        <div class="input-wrapper">
+            <h4>User Age</h4>
+            <input type="range" name="age" min="18" max="50">
+        </div>
+        <div class="input-wrapper">
             <button>Add new user</button>
         </div>
     </form>
